80faf4505e2c41872a889768f4c7f491188e0f03
MarkCuban <83606182+tenmark86@users.noreply.github.com>
2022-09-24 00:18:18 +0300

    uploaded deliverables for site

---
+++ src/components/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Search({ search, setSearch }) {
+  function handleSearch(e){
+    setSearch(e.target.value)
+  }
+  return (
+    <div className="ui large fluid icon input">
+      <input
+        type="text"
+        placeholder="Search your Recent Transactions"
+        onChange={handleSearch}
+      />
+      <i className="circular search link icon"></i>
+    </div>
+  );
+}
+
+export default Search;
